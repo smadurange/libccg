e529ea234fede8794b3b63be7c27a36044a4d59a
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-03-22 23:04:41 +0800

    Define function to fix the heap.

--- src/pqueue_heap.c
+++ src/pqueue_heap.c
@@ -25,6 +25,19 @@ pqueue *ccg_pqueue_create(int (*cmp)(void *, void *)) {
 
 size_t ccg_pqueue_size(const pqueue *pq) { return pq->size; }
 
+static void fix_heap(size_t i, pqueue *pq) {
+  size_t pi;
+  void *tmp, **a;
+  a = pq->heap;
+  while (i > 1 && !pq->cmp(a[i >> 2], a[i])) {
+    pi = i >> 2;
+    tmp = a[i];
+    a[i] = a[pi];
+    a[pi] = tmp;
+    i = pi;
+  }
+}
+
 void ccg_pqueue_insert(void *item, pqueue *pq) {
   int i;
   void **ptr;
@@ -41,7 +54,7 @@ void ccg_pqueue_insert(void *item, pqueue *pq) {
     pq->heap = ptr;
   }
   pq->heap[++pq->size] = item;
-  // todo: fix heap
+  fix_heap(pq->size, pq);
 }
 
 void ccg_pqueue_destroy(pqueue *pq) { ccg_free(pq); }
