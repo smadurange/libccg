114a4c7aaf5c7c3ad15ac7dd1efcfbbe741b1138
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-03-26 20:19:49 +0800

    Fix indentation.

--- src/dict.c
+++ src/dict.c
@@ -9,69 +9,69 @@
 #define TABLEN 100
 
 struct dict {
-  size_t size;
-  size_t tablen;
-  hasher hash;
-  comparer eq;
-  finalizer fin;
-  list **tab;
+	size_t size;
+	size_t tablen;
+	hasher hash;
+	comparer eq;
+	finalizer fin;
+	list **tab;
 };
 
 typedef struct item {
-  void *key;
-  void *val;
+	void *key;
+	void *val;
 } item;
 
 dict *ccg_dict_create(const hasher hf, const comparer eq, const finalizer fin) {
-  dict *dt;
+	dict *dt;
 
-  dt = ccg_malloc(sizeof(dict));
-  dt->size = 0;
-  dt->tablen = TABLEN;
-  dt->hash = hf;
-  dt->eq = eq;
-  dt->fin = fin;
-  dt->tab = 0;
-  return dt;
+	dt = ccg_malloc(sizeof(dict));
+	dt->size = 0;
+	dt->tablen = TABLEN;
+	dt->hash = hf;
+	dt->eq = eq;
+	dt->fin = fin;
+	dt->tab = 0;
+	return dt;
 }
 
 void ccg_dict_put(void *key, void *val, const dict *dt) {
-  item *obj;
+	item *obj;
 
-  obj = ccg_malloc(sizeof(item));
-  obj->key = key;
-  obj->val = val;
-  ccg_list_put_if_absent(obj, dt->eq, dt->tab[dt->hash(key, dt->tablen)]);
+	obj = ccg_malloc(sizeof(item));
+	obj->key = key;
+	obj->val = val;
+	ccg_list_put_if_absent(obj, dt->eq, dt->tab[dt->hash(key, dt->tablen)]);
 }
 
 void *ccg_dict_find(const void *key, const dict *dt) {
-  item obj, *rv;
+	item obj, *rv;
 
-  obj.key = (void *)key;
-  obj.val = 0;
-  rv = ccg_list_find(&obj, dt->eq, dt->tab[dt->hash(key, dt->tablen)]);
-  return rv != 0 ? ((item *)rv)->val : 0;
+	obj.key = (void *)key;
+	obj.val = 0;
+	rv = ccg_list_find(&obj, dt->eq, dt->tab[dt->hash(key, dt->tablen)]);
+	return rv != 0 ? ((item *)rv)->val : 0;
 }
 
 void *ccg_dict_remove(const void *key, const dict *dt) {
-  void *val;
-  item obj, *rv;
+	void *val;
+	item obj, *rv;
 
-  obj.key = (void *)key;
-  obj.val = 0;
-  rv = ccg_list_remove(&obj, dt->eq, &dt->tab[dt->hash(key, dt->tablen)]);
-  if (rv != 0) {
-    val = ((item *)rv)->val;
-    ccg_free(rv);
-    return val;
-  }
-  return 0;
+	obj.key = (void *)key;
+	obj.val = 0;
+	rv = ccg_list_remove(&obj, dt->eq, &dt->tab[dt->hash(key, dt->tablen)]);
+	if (rv != 0) {
+		val = ((item *)rv)->val;
+		ccg_free(rv);
+		return val;
+	}
+	return 0;
 }
 
 void ccg_dict_destroy(dict *dt) {
-  int i;
+	int i;
 
-  for (i = 0; i < dt->tablen; i++)
-    ccg_list_destroy(dt->fin, dt->tab[i]);
-  ccg_free(dt);
+	for (i = 0; i < dt->tablen; i++)
+		ccg_list_destroy(dt->fin, dt->tab[i]);
+	ccg_free(dt);
 }
