083d2621785bb8e9a8adf89b4e855b785a285ddd
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-03-26 16:45:15 +0800

    Define dict interface.

---
+++ src/dict.h
@@ -0,0 +1,14 @@
+#ifndef CCG_DICT_H
+#define CCG_DICT_H
+
+typedef struct dict dict;
+typedef int (*hashcode)(const void *);
+typedef int (*comparer)(const void *, const void *);
+
+dict *ccg_dict_create(const hashcode hc, const comparer eq);
+void *ccg_dict_find(const void *key, const dict *dt);
+void *ccg_dict_remove(const void *key, const dict *dt);
+void ccg_dict_inert(void *key, void *val, const dict *dt);
+void ccg_dict_destroy(dict *dt);
+
+#endif
