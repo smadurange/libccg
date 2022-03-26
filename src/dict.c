bd06238bf46e73b9949b9ea7a90907abaa47c76a
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-03-26 19:48:20 +0800

    wip

--- src/dict.c
+++ src/dict.c
@@ -14,6 +14,11 @@ struct dict {
   list **tab;
 };
 
+typedef struct item {
+  void *key;
+  void *val;
+} item;
+
 dict *ccg_dict_create(const hasher hf, const comparer eq) {
   dict *dt;
 
@@ -27,5 +32,42 @@ dict *ccg_dict_create(const hasher hf, const comparer eq) {
 }
 
 void ccg_dict_put(void *key, void *val, const dict *dt) {
-  ccg_list_put_if_absent(val, dt->eq, dt->tab[dt->hash(key, dt->tablen)]);
+  item *obj;
+
+  obj = ccg_malloc(sizeof(item));
+  obj->key = key;
+  obj->val = val;
+  ccg_list_put_if_absent(obj, dt->eq, dt->tab[dt->hash(key, dt->tablen)]);
+}
+
+void *ccg_dict_find(const void *key, const dict *dt) {
+  item obj, *rv;
+
+  obj.key = (void *)key;
+  obj.val = 0;
+  rv = ccg_list_find(&obj, dt->eq, dt->tab[dt->hash(key, dt->tablen)]);
+  return rv != 0 ? ((item *)rv)->val : 0;
+}
+
+void *ccg_dict_remove(const void *key, const dict *dt) {
+  void *val;
+  item obj, *rv;
+
+  obj.key = (void *)key;
+  obj.val = 0;
+  rv = ccg_list_remove(&obj, dt->eq, &dt->tab[dt->hash(key, dt->tablen)]);
+  if (rv != 0) {
+    val = ((item *)rv)->val;
+    ccg_free(rv);
+    return val;
+  }
+  return 0;
+}
+
+void ccg_dict_destroy(dict *dt) {
+  int i;
+
+  for (i = 0; i < dt->tablen; i++)
+    ccg_list_destroy(dt->tab[i]);
+  ccg_free(dt);
 }
