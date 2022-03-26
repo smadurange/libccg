474a403ac9575b2cce0e352846875a4e07831daf
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-03-26 11:53:54 +0800

    Define unit test.

--- test/list_test.c
+++ test/list_test.c
@@ -44,6 +44,31 @@ START_TEST(test_list_find_or_append) {
 }
 END_TEST
 
+START_TEST(test_list_find) {
+  list *ls;
+  int *a, *b, *c, *d;
+
+  a = ccg_malloc(sizeof(int));
+  b = ccg_malloc(sizeof(int));
+  c = ccg_malloc(sizeof(int));
+  d = ccg_malloc(sizeof(int));
+  *a = 0, *b = 100, *c = 3, *d = 7;
+  ls = ccg_list_create();
+
+  ccg_list_find_or_append(a, (cmp)cmpint, ls);
+  ccg_list_find_or_append(b, (cmp)cmpint, ls);
+  ccg_list_find_or_append(c, (cmp)cmpint, ls);
+  ck_assert_ptr_null(ccg_list_find(d, (cmp)cmpint, ls));
+  ck_assert_int_eq(*(int *)ccg_list_find(b, (cmp)cmpint, ls), *b);
+
+  ccg_free(a);
+  ccg_free(b);
+  ccg_free(c);
+  ccg_free(d);
+  ccg_list_destroy(ls);
+}
+END_TEST
+
 Suite *list_suite() {
   Suite *s;
   TCase *tc;
@@ -53,6 +78,7 @@ Suite *list_suite() {
 
   tcase_add_test(tc, test_list_create);
   tcase_add_test(tc, test_list_find_or_append);
+  tcase_add_test(tc, test_list_find);
 
   suite_add_tcase(s, tc);
   return s;
