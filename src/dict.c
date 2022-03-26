3a44e66d92d530e56f08491b300b948b6cad7f49
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-03-26 17:58:50 +0800

    wip

--- src/dict.c
+++ src/dict.c
@@ -1,2 +1,31 @@
-#include "mem.h"
+#include <stddef.h>
+
 #include "dict.h"
+#include "list.h"
+#include "mem.h"
+
+#define TABLEN 100
+
+struct dict {
+  size_t size;
+  size_t tablen;
+  hash hash;
+  comparer eq;
+  list **tab;
+};
+
+dict *ccg_dict_create(const hash hf, const comparer eq) {
+  dict *dt;
+
+  dt = ccg_malloc(sizeof(dict));
+  dt->size = 0;
+  dt->tablen = TABLEN;
+  dt->hash = hf;
+  dt->eq = eq;
+  dt->tab = 0;
+  return dt;
+}
+
+void ccg_dict_put(void *key, void *val, const dict *dt) {
+  ccg_list_put_if_absent(val, dt->eq, dt->tab[dt->hash(key, dt->tablen)]);
+}
--- src/dict.h
+++ src/dict.h
@@ -1,11 +1,13 @@
 #ifndef CCG_DICT_H
 #define CCG_DICT_H
 
+#include <stddef.h>
+
 typedef struct dict dict;
-typedef int (*hashcode)(const void *);
+typedef int (*hash)(const void *, const size_t n);
 typedef int (*comparer)(const void *, const void *);
 
-dict *ccg_dict_create(const hashcode hc, const comparer eq);
+dict *ccg_dict_create(const hash hf, const comparer eq);
 void ccg_dict_put(void *key, void *val, const dict *dt);
 void *ccg_dict_find(const void *key, const dict *dt);
 void *ccg_dict_remove(const void *key, const dict *dt);
