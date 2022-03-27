cadb75a29946f4a1b08608b0d8f3dc25861317ac
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-03-27 19:44:59 +0800

    Unit test.

--- test/dict_test.c
+++ test/dict_test.c
@@ -1,17 +1,37 @@
-
 #include <check.h>
 
 #include "../src/dict.h"
 #include "../src/mem.h"
 #include "libccg_test.h"
 
+static int hashint(const int *i, const size_t n) { return *i % n; }
+
+static int cmpint(const int *a, const int *b) {
+	if (a == b || *a == *b)
+		return 1;
+	else
+		return 0;
+}
+
+START_TEST(test_dict_create) {
+	dict *dt;
+
+	dt = ccg_dict_create((hasher)hashint, (comparer)cmpint, 0);
+	ck_assert_ptr_nonnull(dt);
+
+	ccg_dict_destroy(dt);
+}
+END_TEST
+
 Suite *dict_suite() {
-  Suite *s;
-  TCase *tc;
+	Suite *s;
+	TCase *tc;
 
-  s = suite_create("dictionary");
-  tc = tcase_create("core");
+	s = suite_create("dictionary");
+	tc = tcase_create("core");
 
-  suite_add_tcase(s, tc);
-  return s;
+	tcase_add_test(tc, test_dict_create);
+
+	suite_add_tcase(s, tc);
+	return s;
 }
