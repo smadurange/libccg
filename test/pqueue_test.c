30ff5a56997cacad345331ba0c7a972e94ae52c1
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-03-28 17:21:16 +0800

    Improve pqueue tests.

--- test/pqueue_test.c
+++ test/pqueue_test.c
@@ -14,45 +14,35 @@ static int cmpint(const int *a, const int *b) {
 		return 1;
 }
 
-static void fin(void *ptr) {
-	ccg_free(ptr);
-}
-
 START_TEST(test_pqueue_create) {
 	pqueue *queue;
 
 	queue = ccg_pqueue_create((comparer)cmpint, 0);
 	ck_assert_ptr_nonnull(queue);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 0);
+
 	ccg_pqueue_destroy(queue);
 }
 END_TEST
 
 START_TEST(test_pqueue_insert_int) {
-	int *a, *b, *c, *d, *e, *f;
+	int a, b, c, d, e, f;
 	pqueue *queue;
 
-	a = ccg_malloc(sizeof(int));
-	b = ccg_malloc(sizeof(int));
-	c = ccg_malloc(sizeof(int));
-	d = ccg_malloc(sizeof(int));
-	e = ccg_malloc(sizeof(int));
-	f = ccg_malloc(sizeof(int));
-	*a = 1, *b = 2, *c = 3, *d = 4, *e = 4, *f = 5;
+	a = 1, b = 2, c = 3, d = 4, e = 4, f = 5;
+	queue = ccg_pqueue_create((comparer)cmpint, 0);
 
-	queue = ccg_pqueue_create((comparer)cmpint, fin);
-
-	ccg_pqueue_insert(a, queue);
+	ccg_pqueue_insert(&a, queue);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 1);
-	ccg_pqueue_insert(b, queue);
+	ccg_pqueue_insert(&b, queue);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 2);
-	ccg_pqueue_insert(c, queue);
+	ccg_pqueue_insert(&c, queue);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 3);
-	ccg_pqueue_insert(d, queue);
+	ccg_pqueue_insert(&d, queue);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 4);
-	ccg_pqueue_insert(e, queue);
+	ccg_pqueue_insert(&e, queue);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 5);
-	ccg_pqueue_insert(f, queue);
+	ccg_pqueue_insert(&f, queue);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 6);
 
 	ccg_pqueue_destroy(queue);
@@ -61,50 +51,51 @@ END_TEST
 
 START_TEST(test_pqueue_remove_int) {
 	pqueue *queue;
-	int *a, *b, *c, *d, *e, *f, *rv;
+	int a, b, c, d, e, f, *rv;
 
-	a = ccg_malloc(sizeof(int));
-	b = ccg_malloc(sizeof(int));
-	c = ccg_malloc(sizeof(int));
-	d = ccg_malloc(sizeof(int));
-	e = ccg_malloc(sizeof(int));
-	f = ccg_malloc(sizeof(int));
-	*a = 1, *b = 8, *c = 3, *d = -1, *e = 5, *f = 3;
-
+	a = 1, b = 8, c = 3, d = -1, e = 5, f = 3;
 	queue = ccg_pqueue_create((comparer)cmpint, 0);
-	ccg_pqueue_insert(a, queue);
-	ccg_pqueue_insert(b, queue);
-	ccg_pqueue_insert(c, queue);
-	ccg_pqueue_insert(d, queue);
-	ccg_pqueue_insert(e, queue);
-	ccg_pqueue_insert(f, queue);
-	
+	ccg_pqueue_insert(&a, queue);
+	ccg_pqueue_insert(&b, queue);
+	ccg_pqueue_insert(&c, queue);
+	ccg_pqueue_insert(&d, queue);
+	ccg_pqueue_insert(&e, queue);
+	ccg_pqueue_insert(&f, queue);
+
 	rv = ccg_pqueue_remove(queue);
-	ck_assert_int_eq(*rv, *b);
+	ck_assert_int_eq(*rv, b);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 5);
 	rv = ccg_pqueue_remove(queue);
-	ck_assert_int_eq(*rv, *e);
+	ck_assert_int_eq(*rv, e);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 4);
 	rv = ccg_pqueue_remove(queue);
-	ck_assert_int_eq(*rv, *c);
+	ck_assert_int_eq(*rv, c);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 3);
 	rv = ccg_pqueue_remove(queue);
-	ck_assert_int_eq(*rv, *f);
+	ck_assert_int_eq(*rv, f);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 2);
 	rv = ccg_pqueue_remove(queue);
-	ck_assert_int_eq(*rv, *a);
+	ck_assert_int_eq(*rv, a);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 1);
 	rv = ccg_pqueue_remove(queue);
-	ck_assert_int_eq(*rv, *d);
+	ck_assert_int_eq(*rv, d);
 	ck_assert_int_eq(ccg_pqueue_size(queue), 0);
 
-	ccg_free(a);
-	ccg_free(b);
-	ccg_free(c);
-	ccg_free(d);
-	ccg_free(e);
-	ccg_free(f);
 	ccg_pqueue_destroy(queue);
+}
+END_TEST
+
+static void fin(void *ptr) { ccg_free(ptr); }
+
+START_TEST(test_pqueue_destroy) {
+	int *a;
+	pqueue *pq;
+
+	a = ccg_malloc(sizeof(int));
+	*a = 1;
+	pq = ccg_pqueue_create((comparer)cmpint, fin);
+	ccg_pqueue_insert(a, pq);
+	ccg_pqueue_destroy(pq);
 }
 END_TEST
 
@@ -118,6 +109,7 @@ Suite *pqueue_suite() {
 	tcase_add_test(tc, test_pqueue_create);
 	tcase_add_test(tc, test_pqueue_insert_int);
 	tcase_add_test(tc, test_pqueue_remove_int);
+	tcase_add_test(tc, test_pqueue_destroy);
 
 	suite_add_tcase(s, tc);
 	return s;
