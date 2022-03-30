eda7e4049fd8a2a8241fa52afc2a9da57b046044
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-03-30 18:18:40 +0800

    Test fixes.

--- test/dict_test.c
+++ test/dict_test.c
@@ -61,9 +61,10 @@ START_TEST(test_dict_find) {
 	ccg_dict_put(k2, &v2, dt);
 	ccg_dict_put(k3, &v3, dt);
 
+	ck_assert_ptr_eq(ccg_dict_find("c", dt), 0);
 	ck_assert_int_eq(*(char *)ccg_dict_find(k1, dt), v1);
 	ck_assert_int_eq(*(char *)ccg_dict_find(k2, dt), v2);
-	ck_assert_int_eq(*(char *)ccg_dict_find(k3, dt), v3);
+	ck_assert_int_eq(*(char *)ccg_dict_find(k3, dt), v2);
 }
 
 Suite *dict_suite() {
