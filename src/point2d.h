bd5953bbddc39f665d2adb4eabc22d455ad14be7
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-04-01 19:35:59 +0800

    Remove point hash function.

--- src/point2d.h
+++ src/point2d.h
@@ -8,10 +8,6 @@
 	(a == b || (fabs(((point2d *)a)->x - ((point2d *)b)->x) < eps &&             \
 							fabs(((point2d *)a)->y - ((point2d *)b)->y) < eps))
 
-#define ccg_point2d_hash(a, n)                                                 \
-	((17 * 31 * (((((point2d *)a)->x + DBL_MAX) / (DBL_MAX + DBL_MAX)) * n)) *   \
-	 31 * (((((point2d *)a)->y + DBL_MAX) / (DBL_MAX + DBL_MAX)) * n))
-
 typedef struct point2d {
 	double x;
 	double y;
