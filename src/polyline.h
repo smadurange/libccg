0e64e8200a0cad79c0ea3e411fd4a2c2a2af5acf
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-04-07 21:32:48 +0800

    wip

---
+++ src/polyline.h
@@ -0,0 +1,6 @@
+#ifndef CCG_POLYLINE_H
+#define CCG_POLYLINE_H
+
+typedef struct polyline polyline;
+
+#endif
--- src/voronoi.h
+++ src/voronoi.h
@@ -1,6 +1,11 @@
 #ifndef CCG_VORONOI2D_H
 #define CCG_VORONOI2D_H
 
+#include "point.h"
+#include "polyline.h"
 
+typedef struct voronoi_diagram voronoi_diagram;
+
+voronoi_diagram *ccg_voronoi_solve(const point **pts, const polyline *bbox);
 
 #endif
