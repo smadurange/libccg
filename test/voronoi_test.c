a55787fa539272f7171447c382d9e4068990d45c
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-04-10 16:11:19 +0800

    wip

--- test/voronoi_test.c
+++ test/voronoi_test.c
@@ -4,18 +4,19 @@
 #include "libccg_test.h"
 
 START_TEST(test_voronoi_solve_basic) {
-	point sites[5], bbox[4];
+	point bbox[4], sites[5];
 	voronoi_diagram *vd;
+
+	bbox[0] = (point){0.0, 2.0};
+	bbox[1] = (point){18.0, 2.0};
+	bbox[2] = (point){18.0, 14.0};
+	bbox[3] = (point){0.0, 14.0};
 
 	sites[0] = (point){6.48, 4.7};
 	sites[1] = (point){13.9, 6.76};
 	sites[2] = (point){7.12, 8.1};
 	sites[3] = (point){12.7, 10.6};
 	sites[4] = (point){4.6, 11.44};
-	bbox[0] = (point){0.0, 2.0};
-	bbox[1] = (point){18.0, 2.0};
-	bbox[2] = (point){18.0, 14.0};
-	bbox[3] = (point){0.0, 14.0};
 
 	vd = ccg_voronoi_solve(sites, 5, bbox);
 
