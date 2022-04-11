9e5f379c53759b7713f0579655a12ec8662931bc
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-04-11 20:58:00 +0800

    Refactor static variables.

--- src/voronoi.c
+++ src/voronoi.c
@@ -95,8 +95,23 @@ typedef struct circle {
 	char deleted;
 } circle;
 
+struct arc {
+	const site *focus;
+	const double *dtx;
+	circle *circles;
+};
+
+typedef struct beachline {
+	arc **arcs;
+	int len;
+	int cap;
+} beachline;
+
 typedef struct event {
 	char site;
+	const double *swp;
+	pqueue *pq;
+	beachline *beach;
 	union {
 		site *s;
 		circle *c;
@@ -121,14 +136,6 @@ static void ev_destroy(event *ev) {
 	ccg_free(ev);
 }
 
-static pqueue *pq;
-static double swp;
-
-struct arc {
-	const site *focus;
-	circle *circles;
-};
-
 static double arc_eval(double x, const arc *a) {
 	// vertical parabola: (x-h)^2 = 4|fy-swp|(y-k)
 	double xh;
@@ -136,7 +143,7 @@ static double arc_eval(double x, const arc *a) {
 
 	f = a->focus->pt;
 	xh = x - f->x;
-	return ((xh * xh) / (4 * (fabs(f->y - swp) / 2))) + (f->y + swp) / 2;
+	return ((xh * xh) / (4 * (fabs(f->y - *a->dtx) / 2))) + (f->y + *a->dtx) / 2;
 }
 
 static inline void arc_destroy(arc *a) {
@@ -149,33 +156,26 @@ typedef struct beachrv {
 	circle **circles;
 } beachrv;
 
-typedef struct beachline {
-	arc **arcs;
-	int len;
-	int cap;
-} beachline;
-
-static beachline beach;
-
 static void breakpoint(const arc *a, const arc *b, point *bp) {
 	// https://github.com/Zalgo2462/VoronoiLib
-	double fax, fay, fbx, fby;
 	const point *fa, *fb;
+	double dtx, fax, fay, fbx, fby;
 
 	fa = a->focus->pt;
 	fb = b->focus->pt;
+	dtx = *a->dtx;
 	fax = fa->x, fay = fa->y;
 	fbx = fb->x, fby = fb->y;
 	bp->x = fabs(fay - fby) < EPSILON
 	          ? (fax + fbx) / 2
-	          : (fbx * (swp - fay) + fax * (fby - swp) +
-	             sqrt((swp - fby) * (swp - fay) *
+	          : (fbx * (dtx - fay) + fax * (fby - dtx) +
+	             sqrt((dtx - fby) * (dtx - fay) *
 	                  ((fbx - fax) * (fbx - fax) + (fby - fay) * (fby - fay)))) /
 	              (fby - fay);
-	bp->y = fabs(fay - swp) < EPSILON ? arc_eval(bp->x, b) : arc_eval(bp->x, a);
+	bp->y = fabs(fay - dtx) < EPSILON ? arc_eval(bp->x, b) : arc_eval(bp->x, a);
 }
 
-static int find_arc_above(const arc *a) {
+static int beachline_find_arc(const arc *a, beachline *beach) {
 	int idx, i;
 	point bp;
 	double x, y, ymin, x0, x1;
@@ -183,16 +183,16 @@ static int find_arc_above(const arc *a) {
 	idx = -1;
 	x0 = -INFINITY, x1 = ymin = INFINITY;
 	x = a->focus->pt->x;
-	for (i = 0; i < beach.len; i++) {
-		y = arc_eval(x, beach.arcs[i]);
+	for (i = 0; i < beach->len; i++) {
+		y = arc_eval(x, beach->arcs[i]);
 		if (y != INFINITY && (fabs(y - ymin) < EPSILON || y < ymin)) {
 			if (i - 1 >= 0) {
-				breakpoint(beach.arcs[i - 1], beach.arcs[i], &bp);
+				breakpoint(beach->arcs[i - 1], beach->arcs[i], &bp);
 				x0 = bp.x;
 			} else
 				x0 = -INFINITY;
-			if (i + 1 < beach.len) {
-				breakpoint(beach.arcs[i], beach.arcs[i + 1], &bp);
+			if (i + 1 < beach->len) {
+				breakpoint(beach->arcs[i], beach->arcs[i + 1], &bp);
 				x1 = bp.x;
 			} else
 				x1 = INFINITY;
@@ -208,59 +208,65 @@ static int find_arc_above(const arc *a) {
 	return idx;
 }
 
-static void beachline_insert_arc(arc *a, beachrv *rv) {
+static void beachline_insert_arc(arc *a, beachrv *rv, beachline *beach) {
 	int i;
 	arc *sa, *lha, *rha;
 
-	if (!beach.arcs) {
-		beach.cap = 4;
-		beach.arcs = ccg_malloc(sizeof(arc *) * beach.cap);
-		beach.arcs[0] = a;
+	if (!beach->arcs) {
+		beach->cap = 4;
+		beach->arcs = ccg_malloc(sizeof(arc *) * beach->cap);
+		beach->arcs[0] = a;
 		return;
 	}
 	i = find_arc_above(a);
 	if (i >= 0) {
-		sa = beach.arcs[i];
+		sa = beach->arcs[i];
 		for (; sa->circles; (sa->circles)++)
 			sa->circles->deleted = 1;
 	}
 	// todo:
 }
 
-static void handle_site_event(site *site, voronoi_diagram *vd) {
+static void beachline_destroy(beachline *beach) { ccg_free(beach); }
+
+static void handle_site_event(event *ev, voronoi_diagram *vd) {
 	arc *a;
 	beachrv rv;
 	edge *e0, *e1;
 
 	rv = (beachrv){};
 	a = ccg_malloc(sizeof(arc));
-	a->focus = site;
+	a->focus = ev->data.s;
 	a->circles = 0;
-	beachline_insert_arc(a, &rv);
+	beachline_insert_arc(a, &rv, ev->beach);
 	if (rv.left) {
 		e0 = ccg_malloc(sizeof(edge));
 		e0->site = rv.left->focus;
 		e1 = ccg_malloc(sizeof(edge));
-		e1->site = site;
+		e1->site = ev->data.s;
 		e0->twin = e1;
 		e1->twin = e0;
 		voronoi_insert_edge(e0, vd);
 		voronoi_insert_edge(e1, vd);
 	}
 	for (; rv.circles; rv.circles++)
-		ccg_pqueue_insert(*rv.circles, pq);
+		ccg_pqueue_insert(*rv.circles, ev->pq);
 }
 
-static void handle_circle_event(circle *circle) {}
+static void handle_circle_event(event *ev) {}
 
 voronoi_diagram *ccg_voronoi_solve(const point *pts, size_t n,
                                    const point *bbox) {
 	int i;
+	double swp;
 	site *s, **sites;
 	event *ev;
+	pqueue *pq;
+	beachline *beach;
 	voronoi_diagram *vd;
 
 	sites = ccg_malloc(sizeof(site *) * (n + 1));
+	beach = ccg_malloc(sizeof(beachline));
 	vd = voronoi_diag_create(sites);
 	pq = ccg_pqueue_create((cmp)evcmp, (cls)ev_destroy);
 	for (i = 0; i < n; i++) {
@@ -269,6 +275,7 @@ voronoi_diagram *ccg_voronoi_solve(const point *pts, size_t n,
 		s->pt = &pts[i];
 		sites[i] = s;
 		ev = ccg_malloc(sizeof(event));
+		ev->swp = &swp;
 		ev->site = 1;
 		ev->data.s = s;
 		ccg_pqueue_insert(ev, pq);
@@ -277,12 +284,13 @@ voronoi_diagram *ccg_voronoi_solve(const point *pts, size_t n,
 	while ((ev = ccg_pqueue_remove(pq))) {
 		if (ev->site) {
 			swp = ev->data.s->pt->y;
-			handle_site_event(ev->data.s, vd);
+			handle_site_event(ev, vd);
 		} else {
 			swp = ev->data.c->center->y - ev->data.c->radius;
-			handle_circle_event(ev->data.c);
+			handle_circle_event(ev);
 		}
 	}
+	beachline_destroy(beach);
 	ccg_pqueue_destroy(pq);
 	return vd;
 }
